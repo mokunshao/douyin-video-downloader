abe9223ab965c0a02594c81391f85c9fca2b8d69
mokunshao <mokunshao@foxmail.com>
2022-06-04 19:35:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+function getDownloadMsg() {
+  const datas = window['SSR_RENDER_DATA'];
+  let target;
+  for (const key in datas) {
+    if (Object.hasOwnProperty.call(datas, key)) {
+      const data = datas[key];
+      if (typeof data === 'object' && data['aweme']) {
+        target = data;
+        break;
+      }
+    }
+  }
+  if (target) {
+    const url = target['aweme']['detail']['video']['bitRateList'][0]['playApi'];
+    const format =
+      target['aweme']['detail']['video']['bitRateList'][0]['videoFormat'];
+    const author = target['aweme']['detail']['authorInfo']['nickname'];
+    const title = target['aweme']['detail']['desc'];
+    if (url) {
+      return { url, author, title, format };
+    }
+  }
+}
+
+function download(url, author, title, format) {
+  fetch(url).then((res) => {
+    res.blob().then((blob) => {
+      var a = document.createElement('a');
+      var urlLink = window.URL.createObjectURL(blob);
+      var filename =
+        'attachment;filename=' + author + '_' + title + '.' + format;
+      a.href = urlLink;
+      a.download = filename.replace('attachment;filename=', '');
+      a.click();
+      window.URL.revokeObjectURL(urlLink);
+    });
+  });
+}
+
+function main() {
+  const { url, author, title, format } = getDownloadMsg();
+  download(url, author, title, format);
+}
+
+main();
